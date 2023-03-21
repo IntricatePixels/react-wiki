b24e5c42edfbb554814a2ab9e9f6f2b79163f443
Taulant Balla <taulant.balla@gmail.com>
2023-03-21 01:26:46 +0000

    Display cards

--- src/App.js
+++ src/App.js
@@ -12,16 +12,29 @@ import Filter from "./components/Filter/Filter";
 import Navbar from "./components/Navbar/Navbar";
 
 
+
 function App() {
+  let api = `https://rickandmortyapi.com/api/character/?page=1`
+  let [fetchedData, updateFetchedData] = useState([]);
+  let { info, results } = fetchedData;
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      updateFetchedData(data);
+    })();
+  }, [api]);
+
+
   return (
     <div className="App">
       <h1 className="text-center mb-3">Characters</h1>
       <div className="container">
         <div className="row">
           Filter component will be placed here
-          <div className="col-lg-8 col-12">
+          <div className="col-lg-12 col-12">
             <div className="row">
-              Card component will be placed here
+              <Card results={results} />
             </div>
           </div>
         </div>
